bc0810409424df84cfef13a91fa7d5b6b2d78727
Yuki Nishijima <mail@yukinishijima.net>
2016-10-15 22:22:30 +0000

    (Hopefully) fix all the builds

--- test/api_client_test.rb
+++ test/api_client_test.rb
@@ -15,21 +15,21 @@ class ApiClientTest < Minitest::Test
     @client.get_users(query: {page: 1})
 
     assert_requested :get, "http://example.org/api/v2/users?page=1",
-                     headers: {'Accept' => 'application/json', 'Content-Type': 'application/json'}
+                     headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
   end
 
   def test_get_all_resources_with_custom_header
-    @client.get_users(query: {page: 1}, headers: {'Accept' => '*/*', 'Content-Type': ''})
+    @client.get_users(query: {page: 1}, headers: {'Accept' => '*/*', 'Content-Type' => ''})
 
     assert_requested :get, "http://example.org/api/v2/users?page=1",
-                     headers: {'Accept' => 'application/json', 'Content-Type': 'application/json'}
+                     headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
   end
 
   def test_get_single_resource
     @client.get_user(1, query: {page: 1})
 
     assert_requested :get, "http://example.org/api/v2/users/1?page=1",
-                     headers: {'Accept' => 'application/json', 'Content-Type': 'application/json'}
+                     headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
   end
 
   def test_post_single_resource
@@ -37,7 +37,7 @@ class ApiClientTest < Minitest::Test
 
     assert_requested :post, "http://example.org/api/v2/users",
                      body: '{"name":"Yuki"}',
-                     headers: {'Accept' => 'application/json', 'Content-Type': 'application/json'}
+                     headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
   end
 
   def test_patch_single_resource
@@ -45,14 +45,14 @@ class ApiClientTest < Minitest::Test
 
     assert_requested :patch, "http://example.org/api/v2/users/1",
                      body: '{"name":"Yuki"}',
-                     headers: {'Accept' => 'application/json', 'Content-Type': 'application/json'}
+                     headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
   end
 
   def test_delete_single_resource
     @client.delete_user(1)
 
     assert_requested :delete, "http://example.org/api/v2/users/1",
-                     headers: {'Accept' => 'application/json', 'Content-Type': 'application/json'}
+                     headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
   end
 
   def test_as_option_oevrrides_method_name
@@ -60,7 +60,7 @@ class ApiClientTest < Minitest::Test
 
     @client.authentication
     assert_requested :get, "http://example.org/authentication",
-                     headers: {'Accept' => 'application/json', 'Content-Type': 'application/json'}
+                     headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
   end
 
   def test_exception_raises_when_timing_out
