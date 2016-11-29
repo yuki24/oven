9d8a327e2098be984dd15c1a2f6f026fe641bed6
Yuki Nishijima <mail@yukinishijima.net>
2016-11-29 02:20:12 +0000

    Refactor a client used in test

--- test/examples/api_client.rb
+++ test/examples/api_client.rb
@@ -1,13 +1,13 @@
 Oven.bake :ApiClient, destination: "tmp/" do
   format :json
 
-  get :users,   "/api/v2/users"
-  get :user,    "/api/v2/users/:id"
-  post :user,   "/api/v2/users"
-  patch :user,  "/api/v2/users/:id"
-  put :user,  "/api/v2/users/:id", as: :put_user
-  delete :user, "/api/v2/users/:id"
-  head :users, "/api/v2/users"
+  get :users,     "/api/v2/users"
+  get :user,      "/api/v2/users/:id"
+  head :users,    "/api/v2/users"
+  post :user,     "/api/v2/users"
+  patch :user,    "/api/v2/users/:id"
+  put :user,      "/api/v2/users/:id", as: :put_user
+  delete :user,   "/api/v2/users/:id"
   options :users, "/api/v2/users"
 
   get :authentication, "/authentication", as: :authentication
